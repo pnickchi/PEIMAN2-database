6c4dd155eb0dc238f89a11543c064b6eaca3a59a
Payman Nickchi <payman.nickchi@gmail.com>
2025-10-09 22:26:34 -0700

    Update

--- run_app.R
+++ run_app.R
@@ -1,16 +1,11 @@
 # Create the log file
-sink( paste0('log_runtime_at_', Sys.Date(), '.txt'), append = TRUE ) 
+sink( paste0('log/log_runtime_at_', Sys.Date(), '.txt'), append = TRUE ) 
 
 # Prints out the run time
 cat( paste0('--- Run at ', Sys.Date(), ' ---') )
 
 # Run the main pipeline
 source('main.R')
-
-#
-system('git add .')
-system( paste0('git commit -m Monthly data update: ', Sys.Date()) )
-system('git push origin main')
 
 # Close log file
 sink()
